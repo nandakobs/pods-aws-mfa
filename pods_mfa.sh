7c5dd18d4c82bc6a4e32252b09f9bbd3ab52b276
Fernanda Kobs <fernanda.is.learning@gmail.com>
2023-09-10 16:12:14 -0300

    refactor: Remove jq dependency

--- pods_mfa.sh
+++ pods_mfa.sh
@@ -70,40 +70,50 @@ remove_aliases() {
 ###      PODS_MFA UTILS [END]       ###
 #######################################
 
+#######################################
+# check_script_setup:
+#   Checks and sets up the necessary requirements for the script to work correctly.
+#   Side effects:
+#     Changes the script's permissions to executable.
+#     Creates or updates a symbolic link to the script.
+#######################################
 check_script_setup() {
-  if [[ ! -x ${0} ]]; then
+  local script_name
+  local script_path
+  local type
+
+  if [[ ! -x "${0}" ]]; then
+    echo_progress  "Making this script executable" 0.2
     chmod +x "${0}"
   fi
 
-  script_name=$(basename "${0}")
-  link_path="/usr/bin/${script_name%.*}"
-  script_path=$(readlink -f "${0}")
+  script_name="$(basename "${0}")"
+  local link_path="/usr/bin/${script_name%.*}"
+  script_path="$(readlink -f "${0}")"
 
   if [[ ! -L "${link_path}" ]] || [[ ! -e "${link_path}" ]]; then
+    echo_progress  "Linking important stuff" 0.2
     sudo ln -sf "${script_path}" "${link_path}"
   else
-    type=$(file -b "${link_path}")
+    type="$(file -b "${link_path}")"
 
     if [[ "${type}" != "symbolic link to ${script_path}" ]]; then
+      echo_progress  "Adjusting some symbolic links" 0.2
       sudo ln -sf "${script_path}" "${link_path}"
     fi
+
   fi
 
-  if ! command -v jq &>/dev/null; then
-    echo "Dependency jq is not installed. Installing..."
-    apt-get update && apt-get install -y jq
-    echo "Dependency jq has been installed."
-  fi
+  echo_progress  "Checking for dependencies" 0.2
+  check_dependency "aws"
 }
 
 remove_script_setup() {
-  sed -i '/^#Pods aliases$/d' "$HOME/.bash_aliases"
-  sed -i '/^alias podsprd=.*/d' "$HOME/.bash_aliases"
-  sed -i '/^alias podsqa=.*/d' "$HOME/.bash_aliases"
-  sed -i '/^alias podsdev=.*/d' "$HOME/.bash_aliases"
-  sed -i '/^export aws_arn=.*/d' "$HOME/.bashrc"
-  sudo apt-get remove --auto-remove jq
+  remove_aliases
+  sed -i "/^export AWS_ARN=.*/d" "$HOME/.bashrc"
+  source "$HOME/.bashrc"
   sudo rm /usr/bin/pods_mfa
+  echo_progress  "Removing pods_mfa" 0.5
 }
 
 write_aliases() {
@@ -155,58 +165,99 @@ manage_aliases() {
   fi
 }
 
+#######################################
+# verify_arn:
+#   Verifies if the AWS_ARN is exported in "$HOME/.bashrc". If not, extracts the value and exports it.
+#   Side effects:
+#     Modifies the "$HOME/.bashrc" file to include the AWS_ARN export if it's not already present.
+#   Related doc:
+#     https://awscli.amazonaws.com/v2/documentation/api/latest/reference/iam/get-user.html
+#     Note that the text output pattern is: "USER    PATH   ARN  USER_ID   CREATE_DATE"
+#######################################
 verify_arn() {
-  # Extract and saves your aws_arn if it's not already set
-  if ! grep -q "export aws_arn=" "$HOME/.bashrc"; then
-    output=$(aws iam get-role --role-name developer 2>&1)
+  local output
+  local arn
 
-    if [[ $output == *"An error occurred"* ]]; then
-      regex="arn:aws:iam::\d+:(mfa|user)/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
-      ARN=$(echo "${output}" | grep -o -P "${regex}")
+  if ! grep -q "export AWS_ARN=" "$HOME/.bashrc"; then
+    output="$(aws iam get-user --output text 2>&1)"
+    arn="$(echo "${output}" | grep -o -P 'arn:aws:iam::[^[:space:]]*')"
 
-      if [[ ${ARN} == *"user"* ]]; then
-        ARN=$(echo "${ARN}" | sed 's/user/mfa/')
+    if [[ -z "${arn}" ]]; then
+
+      if [[ "${arn}" == *":user/"* ]]; then
+        arn="${arn/user/mfa}"
       fi
+
+      echo "export AWS_ARN=\"${arn}\"" >> "$HOME/.bashrc"
+      source "$HOME/.bashrc"
     else
-      ARN=$(echo "${output}" | jq -r '.Role.Arn')
+      err "USER_ARN_NOT_FOUND\n"
+      check_dependency "aws"
+      echo "Please check your configuration in the aws-cli."
+      exit 1
     fi
 
-    if [[ -n "${ARN}" ]]; then
-      echo "export aws_arn=\"${ARN}\"" >>"$HOME/.bashrc"
-      source "$HOME/.bashrc"
-    fi
   fi
 }
 
+#######################################
+# get_new_token:
+#   Asks for the MFA code and tries to get a new session token using it.
+#   If successful, updates the AWS credentials with the new session token.
+#   Inputs:
+#     response_expected - if true, feedback will be provided to the user.
+#   Side effects:
+#     Modifies the AWS credentials with the new session token.
+#     Stores the token expiration date and its timezone in a temporary file.
+#   Related doc:
+#     https://awscli.amazonaws.com/v2/documentation/api/latest/reference/sts/get-session-token.html
+#     Note that the text output pattern is: "CREDENTIALS ACCESS_KEY  EXPIRATION  SECRET_KEY  SESSION_TOKEN"
+#######################################
 get_new_token() {
-  read -r -p "Insert your MFA code: " mfa_code
 
-  number_of_tries=0
-  while [[ $number_of_tries -lt 2 ]]; do
+  while true; do
+    local mfa_code
+    local output
+    local title
+    local try_again
+    local continue
 
-    json_output=$(aws sts get-session-token --serial-number "${aws_arn}" --token-code "${mfa_code}")
+    read -rp "Insert your MFA code: " mfa_code
 
-    echo "${json_output}"
+    while ! [[ "${mfa_code}" =~ ^[0-9]{6}$ ]]; do
+        err "INVALID_CODE\n"
+        read -rp "Insert a valid MFA code: " mfa_code
+    done
 
-    if jq -e '.Credentials | has("SessionToken")' <<<"${json_output}" >/dev/null; then
+    output="$(aws sts get-session-token --serial-number "${AWS_ARN}" --token-code "${mfa_code}" --output text)"
+    title="$(echo "${output}" | awk '{print $1}')"
 
-      access_key=$(echo "$json_output" | jq -r '.Credentials.AccessKeyId')
-      secret_key=$(echo "$json_output" | jq -r '.Credentials.SecretAccessKey')
-      session_token=$(echo "$json_output" | jq -r '.Credentials.SessionToken')
+    if [[ "${title}" == "CREDENTIALS"  ]]; then
+      local access_key
+      local secret_key
+      local session_token
+
+      access_key="$(echo "${output}" | awk '{print $2}')"
+      secret_key="$(echo "${output}" | awk '{print $4}')"
+      session_token="$(echo "${output}" | awk '{print $5}')"
 
       aws configure --profile "mfa" set aws_access_key_id "${access_key}"
       aws configure --profile "mfa" set aws_secret_access_key "${secret_key}"
       aws configure --profile "mfa" set aws_session_token "${session_token}"
 
       break
-
     fi
 
-    number_of_tries=$((number_of_tries + 1))
-    echo "Something went wrong..."
-    read -r -p "Insert your MFA code: " mfa_code
-  done
+    err "${output}"
 
+    read -rp $'\e[1mDo you want to try again?\e[0m [yes/no] ' try_again
+    continue="$(is_input_positive "${try_again}")"
+
+    if [[ "${continue}" == false ]]; then
+      break
+    fi
+
+  done
 }
 
 check_token() {
